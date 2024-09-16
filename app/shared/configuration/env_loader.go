e8a364cd2070f202f7401ab7699908a98280fa45
ignaciojeria <ignaciovl.j@gmail.com>
2024-09-16 20:18:13 -0300

    validate config function moved

--- app/shared/configuration/env_loader.go
+++ app/shared/configuration/env_loader.go
@@ -1,8 +1,11 @@
 package configuration
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
+	"reflect"
+	"strings"
 
 	ioc "github.com/Ignaciojeria/einar-ioc/v2"
 	"github.com/joho/godotenv"
@@ -23,4 +26,28 @@ func NewEnvLoader() EnvLoader {
 
 func (env EnvLoader) Get(key string) string {
 	return os.Getenv(key)
+}
+
+func validateConfig[T any](conf T) (T, error) {
+	var validationErrors []error
+
+	val := reflect.ValueOf(conf)
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Type().Field(i)
+		value := val.Field(i).String()
+
+		requiredTag := field.Tag.Get("required")
+		if requiredTag == "true" && value == "" {
+			validationErrors = append(validationErrors, fmt.Errorf("%s is required but not set", field.Name))
+		}
+	}
+	if len(validationErrors) > 0 {
+		// Convert errors to strings
+		var errorStrings []string
+		for _, err := range validationErrors {
+			errorStrings = append(errorStrings, err.Error())
+		}
+		return conf, fmt.Errorf("configuration errors:\n%s", strings.Join(errorStrings, "\n"))
+	}
+	return conf, nil
 }
