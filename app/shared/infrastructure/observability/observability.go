8e3d9a7b419266789c02c8522ae4f92079d2a3e1
ignaciojeria <ignaciovl.j@gmail.com>
2024-09-19 12:17:49 -0300

    meter provider added

--- app/shared/infrastructure/observability/observability.go
+++ app/shared/infrastructure/observability/observability.go
@@ -4,23 +4,30 @@ import (
 	"log/slog"
 
 	ioc "github.com/Ignaciojeria/einar-ioc/v2"
+	otelmeter "go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/trace"
 )
 
 type Observability struct {
 	Tracer trace.Tracer
 	Logger *slog.Logger
+	Meter  otelmeter.Meter
 }
 
 func init() {
 	ioc.Registry(
 		NewObservability,
 		newTraceProvider,
-		newLoggerProvider)
+		newLoggerProvider,
+		newMeterProvider)
 }
-func NewObservability(tracer trace.Tracer, logger *slog.Logger) Observability {
+func NewObservability(
+	tracer trace.Tracer,
+	logger *slog.Logger,
+	meter otelmeter.Meter) Observability {
 	return Observability{
 		Tracer: tracer,
 		Logger: logger,
+		Meter:  meter,
 	}
 }
