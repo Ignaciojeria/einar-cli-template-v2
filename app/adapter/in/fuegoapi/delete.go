ed50cd23e4c27ded0e49b1b0c55b8b49d8b02f16
ignaciojeria <ignaciovl.j@gmail.com>
2024-11-23 00:38:29 -0300

    missing http fuego methods added

---
+++ app/adapter/in/fuegoapi/delete.go
@@ -0,0 +1,21 @@
+package fuegoapi
+
+import (
+	"archetype/app/shared/infrastructure/fuegoserver"
+
+	ioc "github.com/Ignaciojeria/einar-ioc/v2"
+	"github.com/go-fuego/fuego"
+)
+
+func init() {
+	ioc.Registry(newTemplateDelete, fuegoserver.New)
+}
+func newTemplateDelete(s *fuego.Server) {
+	fuego.Delete(s, "/insert-your-custom-pattern-here", func(c *fuego.ContextWithBody[any]) (any, error) {
+		body, err := c.Body()
+		if err != nil {
+			return "unimplemented", err
+		}
+		return body, nil
+	})
+}
